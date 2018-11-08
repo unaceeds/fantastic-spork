85bc677e8c010e9fa61e7ac214e7c77ccfcb8fa4
unaceeds <michaelgineciv@gmail.com>
2018-11-08 22:07:19 +0100

    new contents of http://www.polsatnews.pl/rss/wszystkie.xml

--- webtrace.sh
+++ webtrace.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 url=$1
+md5=history/$(echo -n "$url" | md5sum | awk '{print $1}').html
 delay=$2
-wget -q -O old.html $url
+if [ ! -e $md5 ]; then
+  wget -q -O $md5 $url
+  git add $md5
+  git commit -m "some new things"
+  git push
+fi
 while (true); do
   wget -q -O new.html $url
-  if diff old.html new.html > /dev/null; then
-    echo "no difference"
-  else
+  if [ "$(diff $md5 new.html)" != "" ]; then
     echo "got some difference"
-    rm old.html
-    mv new.html old.html
-    md5=$(md5sum old.html | awk '{print $1}').html
-    cp old.html $md5
-    git add $md5
+    rm $md5
+    mv new.html $md5
     git commit -m "new contents of $url"
     git push
+  else
+    echo "no difference"
   fi
   sleep $delay
 done
